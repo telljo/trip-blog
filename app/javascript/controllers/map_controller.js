30c2ff779edb19c22e1714751fdf505a56f246b3
Josh Tell <jtell1997@email.com>
2025-03-17 21:36:26 +0530

    Fix some bugs

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,31 +4,31 @@ import maplibregl from 'maplibre-gl';
 export default class extends Controller {
   static targets = ["map"];
 
-  static coordinatesMap = {};
-  static map = null;
-
   connect() {
     if(!this.hasMapTarget) {
       return;
     }
+    this.coordinatesMap = {};
+    this.map = null;
+    this.currentPostId = null;
 
     const mapElement = this.mapTarget;
     mapElement.innerHTML = '';
     const points = JSON.parse(mapElement.dataset.points);
 
     // Initialize coordinatesMap
-    this.constructor.coordinatesMap = points.reduce((acc, point) => {
+    this.coordinatesMap = points.reduce((acc, point) => {
       acc[point.postId] = [point.longitude, point.latitude];
       return acc;
     }, {});
 
     const firstPoint = points[0];
 
-    this.constructor.map = new maplibregl.Map({
+    this.map = new maplibregl.Map({
       container: mapElement,
       style: 'https://api.maptiler.com/maps/hybrid/style.json?key=YceGCelRYIEShW1l58mK',
       center: [firstPoint.longitude, firstPoint.latitude],
-      zoom: 5
+      zoom: 10
     });
 
     points.forEach(point => {
@@ -39,13 +39,13 @@ export default class extends Controller {
           closeOnClick: true,
           closeOnMove: true
          }).setHTML(point.tooltip))
-        .addTo(this.constructor.map);
+        .addTo(this.map);
     });
 
     const coordinates = points.map(point => [point.longitude, point.latitude]);
 
-    this.constructor.map.on('load', () => {
-      this.constructor.map.addSource('route', {
+    this.map.on('load', () => {
+      this.map.addSource('route', {
         'type': 'geojson',
         'data': {
           'type': 'Feature',
@@ -57,7 +57,7 @@ export default class extends Controller {
         }
       });
 
-      this.constructor.map.addLayer({
+      this.map.addLayer({
         'id': 'route',
         'type': 'line',
         'source': 'route',
@@ -70,17 +70,54 @@ export default class extends Controller {
         }
       });
     });
+
+    // Bind to the scroll event
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  handleScroll() {
+    const scrollPosition = window.scrollY + window.innerHeight / 2;
+    let currentPost = null;
+    this.posts = document.querySelectorAll('.post');
+
+    if(this.posts.length > 0 && this.currentPostId === null) {
+      this.currentPostId = this.posts[0].firstElementChild.id.split('_')[1];
+    }
+
+    for (const post of this.posts) {
+      const postTop = post.offsetTop;
+      const postBottom = postTop + post.offsetHeight;
+
+      if (scrollPosition >= postTop && scrollPosition <= postBottom) {
+        const postId = post.firstElementChild.id.split('_')[1];
+        if (this.currentPostId !== postId) {
+          this.currentPostId = postId;
+          currentPost = post;
+          break;
+        }
+      }
+    }
+
+    if (currentPost && this.coordinatesMap[this.currentPostId]) {
+      const [longitude, latitude] = this.coordinatesMap[this.currentPostId];
+      this.map.flyTo({
+        center: [longitude, latitude],
+        zoom: 10,
+        essential: true
+      });
+    }
   }
 
   moveToMarker(event) {
     const postId = event.target.dataset.postId;
 
-    const coordinates = this.constructor.coordinatesMap[postId];
+    const coordinates = this.coordinatesMap[postId];
 
     if (coordinates) {
-      this.constructor.map.flyTo({
+      this.map.flyTo({
         center: coordinates,
-        essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        zoom: 10,
+        essential: true
       });
     } else {
       console.error(`Coordinates not found for postId: ${postId}`);
