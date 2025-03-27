b9676134deb68cfdefcdf813c152fef395f93ba6
Josh Tell <jtell1997@email.com>
2025-03-28 00:08:31 +0530

    Change post form

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -14,6 +14,7 @@ class GeolocationsController < ApplicationController
         "#{loc.geometry['location']['lng']},#{loc.geometry['location']['lat']}"
       ]
     end
+    render json: @geolocations
   end
 
   def find_location
