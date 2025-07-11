fdc187e5f8fd0e64c7983999d13e9552e62bba00
Josh Tell <jtell1997@email.com>
2025-07-11 10:33:04 +0545

    Fix clipboard code

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,17 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = ["content", "button"];
+  static targets = ["content", "tooltip"];
 
   static values = {
-    copyText: { type: String, default: null },
     confirm: { type: String, default: 'Copied' },
     delayMs: { type: Number, default: 2000 }
   }
 
-  copy() {
-    const toolTip = new bootstrap.Tooltip(this.buttonTarget);
-    const text = this.copyTextValue || this.contentTarget?.innerText;
+  copy(event) {
+    const toolTip = new bootstrap.Tooltip(this.tooltipTarget);
+    const textToCopy = event.currentTarget.dataset.clipboardCopyValue
+    const text = textToCopy || this.contentTarget?.innerText;
     if(text) {
       navigator.clipboard.writeText(text).then(() => {
           toolTip.toggle();
@@ -25,12 +25,5 @@ export default class extends Controller {
           console.error('Failed to copy text to clipboard:', error);
         });
     }
-  }
-
-  copyCurrentUrl() {
-    const currentUrl = window.location.href;
-
-    this.copyTextValue = currentUrl;
-    this.copy();
   }
 }
