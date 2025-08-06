4e3c0ee13021f929803c91b597c79d4ef99e128b
Josh Tell <jtell1997@gmail.com>
2025-08-06 18:57:14 +0800

    Further improvements to the search

--- app/models/concerns/post/full_text_search.rb
+++ app/models/concerns/post/full_text_search.rb
@@ -3,30 +3,6 @@ module Post::FullTextSearch
 
   included do
     has_one :post_fts, foreign_key: "rowid"
-
-    after_commit :sync_full_text_search
-  end
-
-  def sync_full_text_search
-    sanitized_body = ActionView::Base.full_sanitizer.sanitize(self.body&.to_s || "")
-
-    quoted_title   = ActiveRecord::Base.connection.quote(self.title.to_s.presence || "")
-    quoted_body    = ActiveRecord::Base.connection.quote(sanitized_body)
-    quoted_country = ActiveRecord::Base.connection.quote(self.country.to_s.presence || "")
-    quoted_city    = ActiveRecord::Base.connection.quote(self.city.to_s.presence || "")
-
-    sql = <<~SQL
-      INSERT OR REPLACE INTO post_fts (rowid, title, body, country, city)
-      VALUES (
-        #{id},
-        #{quoted_title},
-        #{quoted_body},
-        #{quoted_country},
-        #{quoted_city}
-      )
-    SQL
-
-    ActiveRecord::Base.connection.execute(sql)
   end
 
   class_methods do
--- db/migrate/20250711162259_create_virtual_post_fts.rb
+++ db/migrate/20250711162259_create_virtual_post_fts.rb
@@ -1,5 +1,6 @@
 class CreateVirtualPostFts < ActiveRecord::Migration[8.0]
   def up
+    # Creating virtual table post_fts
     execute <<~SQL
       CREATE VIRTUAL TABLE post_fts USING fts5(
         title,
@@ -11,6 +12,39 @@ class CreateVirtualPostFts < ActiveRecord::Migration[8.0]
       );
     SQL
 
+    # Creating trigger post_fts_insert
+    execute <<~SQL
+      CREATE TRIGGER post_fts_insert AFTER INSERT ON posts
+      BEGIN
+        INSERT INTO post_fts (rowid, title, body, country, city)
+        VALUES (NEW.id, NEW.title, NEW.body, NEW.country, NEW.city);
+      END;
+    SQL
+
+    # Creating trigger post_fts_update
+    execute <<~SQL
+      CREATE TRIGGER post_fts_update AFTER UPDATE ON posts
+      BEGIN
+        -- Delete the old entry from the FTS table
+        INSERT INTO post_fts(post_fts, rowid, title, body, country, city)
+        VALUES('delete', old.rowid, old.title, old.body, old.country, old.city);
+
+        -- Insert the new entry into the FTS table
+        INSERT INTO post_fts (rowid, title, body, country, city)
+        VALUES (NEW.id, NEW.title, NEW.body, NEW.country, NEW.city);
+      END;
+    SQL
+
+    # Creating trigger post_fts_delete
+    execute <<~SQL
+      CREATE TRIGGER post_fts_delete AFTER DELETE ON posts
+      BEGIN
+        INSERT INTO post_fts (rowid, title, body, country, city)
+        VALUES (OLD.id, NULL, NULL, NULL, NULL);
+      END;
+    SQL
+
+    # Migrate existing post data
     Post.includes(:rich_text_body).find_each do |post|
       # Fallback to empty strings if any field is nil
       title   = post.title.presence || ""
@@ -35,5 +69,8 @@ class CreateVirtualPostFts < ActiveRecord::Migration[8.0]
 
   def down
     execute "DROP TABLE IF EXISTS post_fts;"
+    execute "DROP TRIGGER IF EXISTS post_fts_insert;"
+    execute "DROP TRIGGER IF EXISTS post_fts_update;"
+    execute "DROP TRIGGER IF EXISTS post_fts_delete;"
   end
 end
